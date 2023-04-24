7bda6bb1bd702c1ce83e62f4e8a1f915b643a2c0
Qyubee99 <101865591+Qyubee99@users.noreply.github.com>
2023-04-24 10:02:08 +0900

    몬스터 무작위 이동 및 플레이어 추적 구현

--- The Binding of Issac/Assets/Scripts/Monster/TrackingPlayer.cs
+++ The Binding of Issac/Assets/Scripts/Monster/TrackingPlayer.cs
@@ -6,20 +6,23 @@ public class TrackingPlayer : MonoBehaviour
 {
 	public float speed;
 
-	Rigidbody2D _rigid;
 	public Rigidbody2D target;
+
+	Rigidbody2D _rigid;
+	SpriteRenderer _spriteRenderer;
 
 	private void Awake()
 	{
 		_rigid = GetComponent<Rigidbody2D>();
+		_spriteRenderer = GetComponent<SpriteRenderer>();
 	}
 	
 	private void FixedUpdate()
 	{
-		// 플레이어 추적 (수정 해야됨)
-		//Vector2 dirVec = target.position - _rigid.position;
-		//Vector2 nextVec = dirVec.normalized * speed * Time.fixedDeltaTime;
-		//_rigid.MovePosition(_rigid.position + nextVec);
-		//_rigid.velocity = Vector2.zero;
+		// 플레이어 추적(수정 해야됨)
+		Vector2 dirVec = target.position - _rigid.position;
+		Vector2 nextVec = dirVec.normalized * speed * Time.fixedDeltaTime;
+		_rigid.MovePosition(_rigid.position + nextVec);
+		_rigid.velocity = Vector2.zero;
 	}
 }
