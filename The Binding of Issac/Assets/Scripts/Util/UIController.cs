ce030ce4b7089cf575912a2935e2580570ab9b9f
Qyubee99 <101865591+Qyubee99@users.noreply.github.com>
2023-05-01 05:34:47 +0900

    메인 타이틀 구현

--- The Binding of Issac/Assets/Scripts/Util/UIController.cs
+++ The Binding of Issac/Assets/Scripts/Util/UIController.cs
@@ -1,15 +1,41 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class UIController : MonoBehaviour
 {
-    void Update()
+
+    public int index;
+    public string levelName;
+
+    AudioSource _audio;
+    public AudioClip _clip;
+    public Animator _fadeAnimator;
+    public Animator _loadAnimator;
+
+	private void Awake()
+	{
+		_audio = GetComponent<AudioSource>();
+	}
+
+	void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            SceneManager.LoadScene("BindingOfIsaac");
+            StartCoroutine(Fading());
         }
+    }
+
+    IEnumerator Fading()
+    {
+		_fadeAnimator.SetTrigger("FadeOut");
+        _audio.Stop();
+        SoundManager._instance.SFXPlay("LoadSFX", _clip);
+        yield return new WaitForSeconds(4.5f);
+
+		_fadeAnimator.SetTrigger("FadeIn");
+		yield return new WaitForSeconds(0.1f);
+		SceneManager.LoadScene(index);
     }
 }
